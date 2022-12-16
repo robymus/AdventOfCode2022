f843691bea020c33972ea1a48122471e58e97acd
robymus <robymus@gmail.com>
2022-12-17 09:01:46 +1100

    day 16

--- day16/day16.rb
+++ day16/day16.rb
@@ -36,25 +36,44 @@ $nodes.each do |node|
 	$path[node] = bfs(node)
 end
 
-# brute force Part 1
+# Part 1
+
+# exhaustive search with simple heuristics
 $best = nil
-$best_path = nil
-def search(open, pos, t, value, pp)
-	# try to open the valve if we have time
-	search(Set.new(open-[pos]), pos, t-1, value+(t-1)*$rate[pos], pp+'*') if open.include?(pos) && t>0 && $rate[pos]>0
-	# go over the possible targets, if we have time
-	$path[pos].each do |dest, path|
-		if t > path.size && open.include?(dest) then
-			search(open, dest, t-path.size, value, pp+dest)
-		end
+$collect_solutions = false
+def search(open, pos, t, value)
+	possible_target = $path[pos].select{|dest, path|path.size<=t-1 && open.include?(dest)}
+	possible_target.each do |dest, path|		
+		search(open - [dest], dest, t-path.size-1, value + (t-path.size-1)*$rate[dest])
 	end
 	# we tried everything possible
 	if !$best || value > $best then
 		$best = value
 		$best_path = pp
-		puts "#{pp} -> #{value}"
+	end
+	# collect solitions by open set, keep best value for each closed set
+	if $collect_solutions then
+		k = $full_set-open.to_set
+		if $all_solutions[k] then
+			$all_solutions[k] = [$all_solutions[k], value].max
+		else
+			$all_solutions[k] = value
+		end
 	end
 end
 
-search($nodes.to_set-['AA'], 'AA', 30, 0, 'AA')
-puts $found.values.max
+search($nodes-['AA'], 'AA', 30, 0)
+puts "Part 1:"
+puts $best
+
+# Part 2
+$best = nil
+$collect_solutions = true
+$full_set = ($nodes-['AA']).to_set
+$all_solutions = {}
+# collect all solutions, indexed by open set
+search($nodes-['AA'], 'AA', 26, 0)
+# find the best combination
+# it's a little slow, ~25 sec, still acceptable
+puts "Part 2:"
+p $all_solutions.keys.combination(2).select{|s1, s2|(s1&s2).empty?}.map{|s1,s2|$all_solutions[s1]+$all_solutions[s2]}.max
